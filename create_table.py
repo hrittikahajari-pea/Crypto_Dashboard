7a56e9609ab3b4699c909ff08278f1af6c3db2b3
Hrittika Hajari <hrittikahajari@gmail.com>
2026-04-29 15:58:07 +0530

    Refactor: moved DB credentials to .env and updated DB connection usage

--- create_table.py
+++ create_table.py
@@ -1,11 +1,13 @@
 import psycopg2
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 conn = psycopg2.connect(
-    dbname="crypto_db",
-    user="postgres",
-    password="hrih",
-    host="localhost",
-    port="5432"
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT")
 )
 
 cur = conn.cursor()
