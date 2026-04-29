eede0503e1a6eabf4f6fba44e20e3ea8dbb2ec72
Hrittika Hajari <hrittikahajari@gmail.com>
2026-04-29 15:46:24 +0530

    Refractor: moved DB credentials to .env and add dotenv support

--- db_connect.py
+++ db_connect.py
@@ -1,13 +1,15 @@
 import psycopg2
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 try:
     conn = psycopg2.connect(
-        dbname="crypto_db",
-        user="postgres",
-        password="hrih",
-        host="localhost",
-        port="5432"
-    )
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT")
+)
 
     print("Connected to database successfully!")
 
